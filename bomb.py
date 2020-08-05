595bc8e230afbaa8e1fd678a083df46a50deeef6
root <root@MIRAGE.localdomain>
2020-08-04 22:12:41 -0400

    finished Whose on First

--- bomb.py
+++ bomb.py
@@ -565,11 +565,71 @@ def wiresequenceMod():
             done = 1
     choose()
 
+def whosonMod():
+    displayDict = {'yes': 3,'first': 2,'display': 6,'okay': 2,'says': 6,
+    'nothing': 3, ' ': 5,'blank': 4,'no': 6,'led': 3,
+    'lead': 6,'read': 4,'red': 4,'reed': 5,'leed': 5,
+    'hold on': 6,'you': 4,'you are': 6,'your': 4,"you're": 4,
+    'ur': 1,'there': 6,"they're": 5,'their': 4,'they are': 3,
+    'see': 6,'c': 2,'cee': 6}
+    tableDict = {'ready': ["yes","okay","what","middle","left","press","right","blank","ready","no","first","uhhh","nothing","wait"],
+    'first': ["left","okay","yes","middle","no","right","nothing","uhhh","wait","ready","blank","what","press","first"],
+    'no': ["blank","uhhh","wait","first","what","ready","right","yes","nothing","left","press","okay","no","middle"],
+    'blank': ["wait","right","okay","middle","blank","press","ready","nothing","no","what","left","uhhh","yes","first"],
+    'nothing': ["uhhh","right","okay","middle","yes","blank","no","press","left","what","wait","first","nothing","ready"],
+    'yes': ["okay","right","uhhh","middle","first","what","press","ready","nothing","yes","left","blank","no","wait"],
+    'what': ["uhhh","what","left","nothing","ready","blank","middle","no","okay","first","wait","yes","press","right"],
+    'uhhh': ["ready","nothing""left","what","okay","yes","ight","no","press","blank","uhhh","middle","wait","first"],
+    'left': ["right","left","first","no","middle","yes","blank","what","uhhh","wait","press","ready","okay","nothing"],
+    'right': ["yes","nothing","ready","press","no","wait","what","right","middle","left","uhhh","blank","okay","first"],
+    'middle': ["blank","ready","okay","what","nothing","press","no","wait","left","middle","right","first","uhhh","yes"],
+    'okay': ["middle","no","first","yes","uhhh","nothing","wait","okay","left","ready","blank","press","what""right"],
+    'wait': ["uhhh","no","blank","okay","yes","left","first","press","what","wait","nothing","ready","right","middle"],
+    'press': ["right","middle","yes","ready","press","okay","nothing","uhhh","blank","left","first","what","no","wait"],
+    'you': ["sure","you are","your","you're","next","uh huh","ur","hold","what?","hold","like","done"],
+    'you are': ["your","next","like","uh huh","what?","done","uh uh","hold","you","u","you're","sure","ur","you are"],
+    'your': ["uh uh","you are","uh huh","your","next","ur","sure","u","you're","you","what?","hold","like","done"],
+    "you're": ["you","you're","ur","next","uh uh","you are","u","your","what?","uh huh","sure","done","like","hold"],
+    'ur': ["done","u","ur","uh huh","what?","sure","your","hold","you're","like","next","uh uh","you are","you"],
+    'u':["uh huh","sure","next","what?","you're","ur","uh uh","done","u","you","like","hold","you are","your"],
+    'uh huh':["uh huh","your","you are","you","done","hold","uh uh","next","sure","like","you're","ur","u","what?"],
+    'uh uh':["ur","u","you are","you're","next","uh uh","done""you","uh huh","like","your","sure","hold","what?"],
+    'what?':["you","hold","you're","your","u","done","uh uh","like","you are","uh huh","ur","next","what?","sure"],
+    'done':["sure","uh huh","next","what?","your","ur","you're","hold","like","you","u","you are","uh uh","done"],
+    'next':["what?","uh huh","uh uh","your","hold","sure","next","like","done","you are","ur","you're","u","you"],
+    'hold':["you are","u","done","uh uh","you","ur","sure","what?","you're","next","hold","uh huh","your","like"],
+    'sure':["you are","done","like","you're","you","hold","uh huh","ur","sure","u","what?","next","your","uh uh"],
+    'like':["you're","next","u","ur","hold","done","uh uh","what?","uh huh","you","like","sure","you are","your"]}
+    done = 0
+    while not done:
+        display = input("\nPlease input word on display, please enter ' ' for a blank display:\n")
+        if display == 'd':
+            break
+        label = list(displayDict.values())[list(displayDict.keys()).index(display)]
+        if label == 1:
+            position = input("\nPlease input word on label in the TOP LEFT:\n")
+        elif label == 2:
+            position = input("\nPlease input word on label in the TOP RIGHT:\n")
+        elif label == 3:
+            position = input("\nPlease input word on label in the MIDDLE LEFT:\n")
+        elif label == 4:
+            position = input("\nPlease input word on label in the MIDDLE RIGHT:\n")
+        elif label == 5:
+            position = input("\nPlease input word on label in the BOTTOM LEFT:\n")
+        else:
+            position = input("\nPlease input word on label in the BOTTOM RIGHT:\n")
+        #sanitize position
+        table = tableDict[position]
+        print("\nPlease press the first word in this list:")
+        print(', '.join(table))
+        print("\n")
+    choose()
+
 def mazeMod():
     print("WIP")
     choose()
 
-def passMod()
+def passMod():
     print("WIP")
     choose()
 
@@ -592,12 +652,14 @@ def module(num):
         mazeMod()
     elif num is 9:
         passMod()
+    elif num is 10:
+        whosonMod()
     elif num is 0:
         print("\nComplete")
         return
         
 def choose():
-    choice = input("\nPlease enter module selection:\n1)\tWires\n2)\tButton\n3)\tSimon Says\n4)\tMemory\n5)\tMorse\n6)\tComplicated Wires\n7)\tWire Sequences\n8)\tMaze\n9)\tPassword\n0)\tExit\n")
+    choice = input("\nPlease enter module selection:\n1)\tWires\n2)\tButton\n3)\tSimon Says\n4)\tMemory\n5)\tMorse\n6)\tComplicated Wires\n7)\tWire Sequences\n8)\tMaze\n9)\tPassword\n10)\tWhose on First\n0)\tExit\n")
     choice = int(choice)
     module(choice)
 
